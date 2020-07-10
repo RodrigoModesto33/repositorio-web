2ef8046645a0bf972dbb950cb2f84edf9560bd45
RodrigoModesto33 <42869606+RodrigoModesto33@users.noreply.github.com>
2020-07-10 10:47:02 -0300

    Projetos de web+express
    Projetos de web+express

---
+++ express/services/EstudanteService.js
@@ -0,0 +1,51 @@
+const EstudanteModel = require('../models/EstudanteModel')
+
+let estudantes = [
+    {_id:0,nome:'beltrano',curso:'SI',IRA:'8'},
+    {_id:1,nome:'Cicrano',curso:'SI',IRA:'8'},
+    {_id:2,nome:'Beltrano',curso:'SI',IRA:'8'}
+]
+let _id = 3
+class EstudanteService {
+    static register(data) {
+        let estudante = new EstudanteModel(
+            _id++,
+            data.nome,
+            data.curso,
+            data.IRA);
+        estudantes.push(estudante);
+        return estudante;
+    }
+    static list() {
+        return estudantes;
+    }
+    static update(_id, data) {
+        for (let e of estudantes) {
+            if (e._id == _id) {
+                e.nome = data.nome
+                e.curso = data.curso
+                e.IRA = data.IRA
+                return e;
+            }
+        }
+        return null;
+    }
+    static delete(_id) {
+        for (let i = 0; i < estudantes.length; i++) {
+            if (estudantes[i]._id == _id) {
+                estudantes.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+    static retrieve(_id) {
+        for (let i = 0; i < estudantes.length; i++) {
+            if (estudantes[i]._id == _id) {
+                return estudantes[i];
+            }
+        }
+        return {};
+    }
+}
+module.exports = EstudanteService
